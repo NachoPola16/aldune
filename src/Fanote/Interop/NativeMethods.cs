16f7f8a10db6c3692d3576b98cc9e131e7f4a95a
NachoPola16 <nachopolac@gmail.com>
2026-08-30 15:49:05 +0200

    Add WS_EX_NOACTIVATE interop wrapper

---
+++ src/Fanote/Interop/NativeMethods.cs
@@ -0,0 +1,21 @@
+using System.Runtime.InteropServices;
+
+namespace Fanote.Interop;
+
+internal static class NativeMethods
+{
+    private const int GWL_EXSTYLE = -20;
+    private const int WS_EX_NOACTIVATE = 0x08000000;
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+    internal static void MakeNonActivating(IntPtr hWnd)
+    {
+        int exStyle = GetWindowLong(hWnd, GWL_EXSTYLE);
+        SetWindowLong(hWnd, GWL_EXSTYLE, exStyle | WS_EX_NOACTIVATE);
+    }
+}
